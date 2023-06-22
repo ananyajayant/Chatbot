80d7ee248daa6263e0a7c90d52ee9c97bf05d381
Ananya Budhauliya <87610762+ananyajayant@users.noreply.github.com>
2023-06-22 12:56:57 +0530

    Create my_chatbot_logic.py

---
+++ my_chatbot_logic.py
@@ -0,0 +1,4 @@
+def generate_response(message):
+    # Add your chatbot logic here, using the message as input
+    response = "This is a response to the message: {}".format(message)
+    return response
